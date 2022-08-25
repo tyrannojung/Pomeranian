c4e269df36a01f6a7c88039d97c8d9043329fd91
jungdawoon <tyranno2020@gmail.com>
2022-08-25 22:29:45 +0900

    update

--- extension/src/js/backupABI.js
+++ extension/src/js/backupABI.js
@@ -1,4 +1,39 @@
 const backupABI = [
-	"function setBackupData(address key, string backup)",
-	"function getBackupData(address key) view returns (string)"
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "key",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "backup",
+				"type": "string"
+			}
+		],
+		"name": "setBackupData",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "key",
+				"type": "address"
+			}
+		],
+		"name": "getBackupData",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
 ]
--- extension/src/js/test.js
+++ extension/src/js/test.js
@@ -1,23 +1,23 @@
 $(async function () {
 
-    // var randomSeed = ethers.Wallet.createRandom();
-    // console.log(randomSeed);
+    var randomSeed = ethers.Wallet.createRandom();
+    console.log(randomSeed);
 
-    // var address = randomSeed.address;
-    // var mnemonic = randomSeed.mnemonic;
-    // var privateKey = randomSeed.privateKey;
+    var address = randomSeed.address;
+    var mnemonic = randomSeed.mnemonic.phrase;
+    var privateKey = randomSeed.privateKey;
 
-    // console.log(address);
-    // console.log(mnemonic);
-    // console.log(privateKey);
+    console.log(address);
+    console.log(mnemonic);
+    console.log(privateKey);
 
-    // // private key로 지갑에 대한 모든 정보 get
-    // let wallet = new ethers.Wallet(privateKey);
-    // console.log(wallet)
+    // private key로 지갑에 대한 모든 정보 get
+    let wallet = new ethers.Wallet(privateKey);
+    console.log(wallet)
 
-    // // mnemonic key로 지갑에 대한 모든 정보 get
-    // let mnemonicWallet = ethers.Wallet.fromMnemonic(mnemonic);
-    // console.log(mnemonicWallet);
+    // mnemonic key로 지갑에 대한 모든 정보 get
+    let mnemonicWallet = ethers.Wallet.fromMnemonic(mnemonic);
+    console.log(mnemonicWallet);
 
     // // 검증
     // try {
@@ -40,24 +40,33 @@ $(async function () {
     // provider.getGasPrice().then((gasPrice) => {
     //     // gasPrice is a BigNumber; convert it to a decimal string
     //     gasPriceString = gasPrice.toString();
-    
+
     //     console.log("Current gas price: " + gasPriceString);
     // });
 
     // get
-    var contractAddress = "0xC7ec28a973644F50DeE0EAB5E6e7e08d907D26DC";
+    var contractAddress = "0x51a652a26B2BA5276321565B2ca4860184634940";
     const backupContract = new ethers.Contract(contractAddress, backupABI, provider);
     let currentValue = await backupContract.getBackupData("0xFCED72598d28d6e1AE8f4fA1CCB8b10669205823");
     console.log(currentValue);
 
-    var adkey = "0x75845B0f972413C3f9D0B846f5e0F3fE473761a5";
-    var pkey = "0xc6db8ab944d39645292d9ee615ad4c0321d14d1b7a92f47a768568456f5413a6";
-    var bdata = "QmYPzADjrz1GV9M7MBYDthi7RwXRpBRpZZmF6y12YkNrRr"
+    var adkey = "0xFCED72598d28d6e1AE8f4fA1CCB8b10669205823";
+    var pkey = "0xa00f4235f6ad306b79d134fc185267489201fcc3d58eef5b245480f21509a73e";
+    var bdata = "QmYPzADjrz1GV9M7MBYDthi7RwXRpBRpZZmF6y12YAAAAA"
 
-    let wallet = new ethers.Wallet(pkey, provider);
-    const backupContract2 = new ethers.Contract(contractAddress, backupABI, wallet);
-    const estimatedGasLimit = await backupContract2.setBackupData(adkey, bdata); // approves 1 USDT
+    let wallet1 = new ethers.Wallet(pkey, provider);
+    const backupContract2 = new ethers.Contract(contractAddress, backupABI, wallet1);
 
+    adkey = "0x75845B0f972413C3f9D0B846f5e0F3fE473761a5";
+
+    const estimatedGasLimit = await backupContract2.estimateGas.setBackupData(adkey, bdata);
+    const gasPrice = await provider.getGasPrice();
+    const finalGasPrice = gasPrice * estimatedGasLimit;
+    console.log(ethers.utils.formatUnits(finalGasPrice));
+
+    //성공햇드아.
+    //const result = await backupContract2.setBackupData(adkey, bdata); // approves 1 USDT
+    //console.log(result);
 
 
     // Web3.js
